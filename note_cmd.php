e79edd2f1a547447ce0723996df3f9f6321563e5
Dao Nhat Thanh [PG] <nhat_thanh@lampart-vn.com>
2018-10-15 14:57:15 +0700

    database seed

---
+++ note_cmd.php
@@ -0,0 +1,13 @@
+run create table:
+    php artisan migrate
+reset all command migration :
+    php artisan migrate:reset  or php artisan mirgate:rollback --step=5
+delete all table & run command migrate :
+    php artisan migrate:fresh
+
+//========DB Seeders================
+php artisan make:seeder UsersTableSeeder
+    composer dump-autoload
+    php artisan db:seed
+    php artisan db:seed --class=UsersTableSeeder
+    php artisan migrate:refresh --seed
