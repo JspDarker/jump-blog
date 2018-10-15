712e098506204ec2fb856b1599b966ca66a7dcff
Dao Nhat Thanh [PG] <nhat_thanh@lampart-vn.com>
2018-10-15 08:38:18 +0700

    add hello world

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@
 |
 */
 
-Route::get('well', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('hello');
 });
 
-Route::group(['prefix' => 'dev'], function () {
+Route::group(['prefix' => 'dev', 'middleware' => 'web'], function () {
    Route::get('param', 'LearnRouteController@index')->name('param');
    Route::get('go/{id}', 'LearnRouteController@go')->name('go.param');
 });
